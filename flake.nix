ec4688db756cc52b5e2bf3f987295ef178b6ba2c
caio86 <caioluiz86@gmail.com>
2025-04-02 16:48:38 -0300

    flake: implement flake-parts and add apps for nix run

--- flake.nix
+++ flake.nix
@@ -1,66 +1,85 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      javaVersion = 17;
-
+    { self, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs systems (
-          system:
-          function (
-            import nixpkgs {
-              inherit system;
-              overlays = [ self.overlays.default ];
-            }
-          )
-        );
-    in
-    {
-      overlays.default =
-        final: prev:
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                gcc
+                jdk
+                maven
+                ncurses
+                patchelf
+                zlib
+              ];
+
+              shellHook =
+                let
+                  loadLombok = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
+                  prev = "\${JAVA_TOOL_OPTIONS:+ $JAVA_TOOL_OPTIONS}";
+                in
+                ''
+                  export JAVA_TOOL_OPTIONS="${loadLombok}${prev}"
+                '';
+            };
+          };
+
+          packages = {
+            order-ms = pkgs.callPackage ./nix/derivation.nix { };
+          };
+
+          apps = {
+            order-ms = {
+              type = "app";
+              program = "${self'.packages.order-ms}/bin/order-ms";
+            };
+          };
+
+        };
+
+      flake =
         let
-          jdk = prev."jdk${toString javaVersion}";
+          javaVersion = 17;
         in
         {
-          inherit jdk;
-          maven = prev.maven.override { jdk_headless = jdk; };
-          gradle = prev.gradle.override { java = jdk; };
-          lombok = prev.lombok.override { inherit jdk; };
-        };
-
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            gcc
-            jdk
-            maven
-            ncurses
-            patchelf
-            zlib
-          ];
-
-          shellHook =
+          overlays.default =
+            final: prev:
             let
-              loadLombok = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
-              prev = "\${JAVA_TOOL_OPTIONS:+ $JAVA_TOOL_OPTIONS}";
+              jdk = prev."jdk${toString javaVersion}";
             in
-            ''
-              export JAVA_TOOL_OPTIONS="${loadLombok}${prev}"
-            '';
-        };
-      });
+            {
+              inherit jdk;
+              maven = prev.maven.override { jdk_headless = jdk; };
+              gradle = prev.gradle.override { java = jdk; };
+              lombok = prev.lombok.override { inherit jdk; };
+            };
 
-      packages = forAllSystems (pkgs: {
-        order-ms = pkgs.callPackage ./nix/derivation.nix { };
-      });
+        };
     };
 }
